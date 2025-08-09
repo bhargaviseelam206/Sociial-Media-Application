a80eb90fececba222eab59446c3add68cf7ee047
Harshi679 <rharshitha417@gmail.com>
2025-08-09 10:10:01 +0530

    added the components and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Messages from './pages/Messages'
+import Feed from './pages/Feed'
+import Login from './pages/Login'
+import ChatBox from './pages/ChatBox'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}>
+          <Route index element={<Feed/>}/>
+           <Route path='messages' element={<Messages/>}/>
+            <Route path='messages/:userId' element={<ChatBox/>}/>
+            <Route path='messages' element={<Messages/>}/>
+            <Route path='discover' element={<Discover/>}/>
+            <Route path='profile' element={<Profile/>}/>
+            <Route path='Profile/:profileId' element={<Messages/>}/>
+            <Route path='create-post' element={<CreatePost/>}/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
