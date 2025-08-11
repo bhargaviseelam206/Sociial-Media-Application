01e7a628de84c9b798e6651d5082f132c60db63c
Harshi679 <rharshitha417@gmail.com>
2025-08-11 17:05:25 +0530

    added storymodal,storyviewer and postcard components

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { dummyStoriesData} from '../assets/assets' 
 import { Plus } from 'lucide-react'
 import moment from 'moment'
-
+import StoryModal from './StoryModal'
+import StoryViewer from './StoryViewer'
 const StoriesBar = () => {
 
     const [stories, setStories] = useState([])
-
+    const [showModal, setShowModal] = useState(false)
+    const [viewStory, setViewStory] = useState(null)
     const fetchStories = async () => {
         setStories(dummyStoriesData)
     }
@@ -19,7 +21,7 @@ const StoriesBar = () => {
     <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4'>
         <div className='flex gap-4 pb-5'>
             { /* Add Story Card */}
-            <div className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200
+            <div onClick={()=>setShowModal(true)} className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200
             border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
                 <div className='h-full flex flex-col items-center justify-center p-4'>
                     <div className='size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3'>
@@ -33,7 +35,7 @@ const StoriesBar = () => {
             { /* Story Cards */}
             {
                 stories.map((story, index)=> (
-                    <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all
+                    <div onClick={()=>setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all
                     duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700
                     hover:to-purple-800 active:scale-95`}>
                         <img src={story.user.profile_picture} alt="" 
@@ -60,6 +62,10 @@ const StoriesBar = () => {
             }
         </div>
       
+      {/* Add story Model*/}
+      {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories}/>}
+      {/* View Story Modal */}
+      {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory}/>}
     </div>
   )
 }
