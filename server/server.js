64161ae2273b07f80769656b4dc15a43cb4bca4e
Bhargavi_Seelam <bhargaviseelam2006@gmail.com>
2025-08-20 01:52:12 +0530

    "server.js resolved"

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/',(req,res)=>res.send('server is running'))
-app.use('/api/inngest',serve({ client: inngest, functions }))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 const PORT =process.env.PORT || 4000;
 
