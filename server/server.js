bac72fd11eef2842e71a51cd4cb9a8de55364658
Bhargavi_Seelam <bhargaviseelam2006@gmail.com>
2025-08-26 01:44:23 +0530

    "Added postRoutes and functions of likepost and unlikepost "

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { inngest, functions } from "./src/inngest/index.js"
 import { serve } from "inngest/express";  
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRouter.js';
+import postRouter from './routes/postRouter.js';
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(clerkMiddleware());
 app.get('/',(req,res)=>res.send('server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
+app.use('/api/post', postRouter)
 
 const PORT =process.env.PORT || 4000;
 
