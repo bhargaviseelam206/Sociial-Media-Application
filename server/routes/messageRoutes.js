75a0ba9a8cb158b323f0cf7dede3a0ea40b17fc2
Bhargavi_Seelam <bhargaviseelam2006@gmail.com>
2025-09-04 14:47:37 +0530

    "Resolved Errors"

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { sendMessage, sseController } from '../controllers/messageController.js';
+import { getChatMessages, sendMessage, sseController } from '../controllers/messageController.js';
 import { upload } from '../configs/multer.js';
 import { protect } from '../middlewares/auth.js';
 
 const messageRouter = express.Router();
 
 messageRouter.get('/:userId', sseController)
-messageRouter.post('/send', upload.single('image'), ProcessingInstruction, sendMessage)
+messageRouter.post('/send', upload.single('image'), sendMessage)
 messageRouter.post('/get', protect, getChatMessages )
 
 export default messageRouter
